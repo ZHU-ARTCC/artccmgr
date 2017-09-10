ef53bcff6b32c5f6dc8663d79bda27d83f4f904a
Jonathan P. Voss <jvoss@onvox.net>
2017-09-10 00:10:48 -0500

    basic user authentication

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,13 +4,27 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_by(cid: request.env['omniauth.auth'].info.id.to_i)
 
     if @user.present?
-      validate @user, request.env['omniauth.auth']
-
+      # validate @user, request.env['omniauth.auth']
       sign_in_and_redirect @user, event: :authentication
     else
       reason = "Member not found on website"
       set_flash_message(:notice, :failure, kind: 'VATSIM', reason: reason)
-      redirect_to root_path
+      # @vatsim_data = request.env['omniauth.auth']
+
+      session['devise.vatsim.cid']          = request.env['omniauth.auth'].info.id
+      session['devise.vatsim.name_first']   = request.env['omniauth.auth'].info.name_first
+      session['devise.vatsim.name_last']    = request.env['omniauth.auth'].info.name_last
+      session['devise.vatsim.rating']       = request.env['omniauth.auth'].info.rating
+      session['devise.vatsim.pilot_rating'] = request.env['omniauth.auth'].info.pilot_rating
+      session['devise.vatsim.email']        = request.env['omniauth.auth'].info.email
+      session['devise.vatsim.experience']   = request.env['omniauth.auth'].info.experience
+      session['devise.vatsim.reg_date']     = request.env['omniauth.auth'].info.reg_date
+      session['devise.vatsim.country']      = request.env['omniauth.auth'].info.country
+      session['devise.vatsim.region']       = request.env['omniauth.auth'].info.region
+      session['devise.vatsim.division']     = request.env['omniauth.auth'].info.divison
+      session['devise.vatsim.subdivision']  = request.env['omniauth.auth'].info.subdivison
+
+      redirect_to new_user_path
     end
   end
 
