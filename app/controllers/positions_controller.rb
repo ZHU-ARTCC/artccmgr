68252a52c8808b53833af6e4ba3f1fd560438af1
Jonathan P. Voss <jvoss@onvox.net>
2017-09-11 22:04:11 -0500

    code cleanup

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,6 +1,6 @@
 class PositionsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  after_action :verify_authorized#, only: [:create, :new, :update, :destroy]
+  after_action :verify_authorized
 
   def index
     @positions = Position.all
