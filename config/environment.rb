632564e3155095d0d374324f438357e473eafc56
Jonathan P. Voss <jvoss@onvox.net>
2017-09-15 17:32:30 -0500

    commented out METAR as it needs serious re-work on how it operates. Worked on feedback form.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Turn off field_with_errors wrappers
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
