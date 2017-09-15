729979e533fd36f90d1f64cabd70ffca981fe876
Jonathan P. Voss <jvoss@onvox.net>
2017-09-15 18:36:59 -0500

    change to caching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
   def index
     @events = Event.where('end_time > ?', Time.now).order(start_time: :asc).limit(3)
 
-    Rails.cache.fetch('news-feed', expires_in: 30.minutes) do
-      @news = RSS::Parser.parse(open(Artccmgr::Application::RSS_FEED_URL).read, false).items[0..5]
+    @news = Rails.cache.fetch('news-feed', expires_in: 30.minutes) do
+      puts "Here @ #{Time.now}"
+      RSS::Parser.parse(open(Artccmgr::Application::RSS_FEED_URL).read, false).items[0..5]
     end
   end
 
