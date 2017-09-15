a3f3add89db0778a96ddba47c4886a42946d4d1c
Jonathan P. Voss <jvoss@onvox.net>
2017-09-15 11:59:26 -0500

    updated event stuff and added check for event to end after it begins

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,15 @@ class Event < ApplicationRecord
   validates :start_time, presence: true, allow_blank: false, time: true
   validates :end_time, presence: true, allow_blank: false, time: true
 
+  validate :ends_after_start
+
+  private
+
+  # Validates the end time is after the start time
+  def ends_after_start
+    unless end_time.nil? || start_time.nil?
+      errors[:end_time] << 'cannot be before start time' if end_time < start_time
+    end
+  end
+
 end
