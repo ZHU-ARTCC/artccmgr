b12d6bb645feaf61545761fbede1aa555b1efd77
Jonathan P. Voss <jvoss@onvox.net>
2017-10-19 16:54:23 -0500

    VATSIM SSO data is now reconciled with the user on login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,34 +1,59 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
+  # Callback method to handle SSO failures
+  #
   def failure
     set_flash_message(:notice, :failure, kind: 'VATSIM', reason: 'OAuth failure.')
     redirect_to root_path
   end
 
+  # Callback method to handle the SSO callback from Omniauth/VATSIM SSO
+  #
   def vatsim
+    sso_data = request.env['omniauth.auth']
+    sso_info = sso_data.info
+
     @user = User.find_by(cid: request.env['omniauth.auth'].info.id.to_i)
 
     if @user.present?
-      #@user.validate_vatsim_data(request.env['omniauth.auth'])
+      # Check for changes since last login
+      reconcile_vatsim_data(@user, sso_info)
+
+      # Sign in to website
       sign_in_and_redirect @user, event: :authentication
     else
       #reason = "Member not found on website"
       #set_flash_message(:notice, :failure, kind: 'VATSIM', reason: reason)
 
-      sso_info = request.env['omniauth.auth'].info
+      # Create a new User object
+      @user = User.new
 
-      @user = User.create(
-               cid:         sso_info.id.to_i,
-               name_first:  sso_info.name_first,
-               name_last:   sso_info.name_last,
-               email:       sso_info.email,
-               rating:      Rating.find_by(number: sso_info.rating.id.to_i),
-               reg_date:    sso_info.reg_date,
-               group:       Group.find_by(name: 'Guest')
-      )
+      # Update the User object with VATSIM SSO data
+      reconcile_vatsim_data(@user, sso_info)
 
+      # Sign in to website
       sign_in_and_redirect @user, event: :authentication
     end
+  end
+
+  private
+
+  # Reconcile VATSIM data with local user data
+  #
+  def reconcile_vatsim_data(user, vatsim)
+    # If this is not a new user, we do not want to change CID or Group
+    unless user.persisted?
+      user.cid      = vatsim.cid
+      user.group    = Group.find_by(name: 'Guest')
+    end
+
+    user.name_first = vatsim.name_first
+    user.name_last  = vatsim.name_last
+    user.email      = vatsim.email
+    user.rating     = Rating.find_by(number: vatsim.rating.id.to_i)
+    user.reg_date   = vatsim.reg_date
+
+    user.save if user.changed?
   end
 
 end
