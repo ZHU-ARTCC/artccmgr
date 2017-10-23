6e01d7c6261777f6d7cb0feea3c15efd25c2a504
Jonathan P. Voss <jvoss@onvox.net>
2017-10-22 20:44:55 -0500

    changed fake VATUSA trigger to be based on key setting of 'FAKE_KEY' instead of URL to allow staging in Heroku

--- config/initializers/vatusa_api.rb
+++ config/initializers/vatusa_api.rb
@@ -2,7 +2,7 @@ require_relative '../../spec/support/fake_vatusa_api'
 
 # Use the Mock VATUSA API. Even if set to api.vatusa.net by accident in test,
 # the rails_helper will still route requests to the FakeVATUSAAPI and fail!
-if Rails.application.secrets.vatusa_api_url == 'http://localhost:3000/fakeapi/VATUSA'
+if Rails.application.secrets.vatusa_api_key == 'FAKE_KEY'
 	puts 'WARNING: Using Fake VATUSA API'
 	Rails.application.config.middleware.use FakeVATUSAAPI
 end
